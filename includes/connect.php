7ea554c79ab2d3d8a6aa53f1f8ab820aa60a849b
silaganreal <34265179+silaganreal@users.noreply.github.com>
2024-09-23 10:13:50 +0800

    Update connect.php

--- includes/connect.php
+++ includes/connect.php
@@ -1,8 +1,8 @@
 <?php
-$host = 'localhost';
-$user = 'root';
-$pass = '';
-$data = 'petcare';
+$host = 'your_host';
+$user = 'your_user';
+$pass = 'your_pass';
+$data = 'your_data';
 
 $conn = mysqli_connect($host, $user, $pass, $data);
 ?>
